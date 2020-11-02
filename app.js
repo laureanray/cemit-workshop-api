0b2c564536543fd854028519ca144f006f64d3a9
laureanray <laureanraybahala@gmail.com>
2020-11-03 05:21:15 +0800

    change port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 
-const port = 80
+const port = process.env.PORT || 80
 
 app.get('/', (req, res) => {
     res.send({msg: "hello"});
