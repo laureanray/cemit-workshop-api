538c080abf19d1e3513b736ba446898874e73746
laureanray <laureanraybahala@gmail.com>
2020-11-03 05:14:36 +0800

    update

--- app.js
+++ app.js
@@ -2,21 +2,10 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 
-const redis = require("redis");
-const client = redis.createClient();
-
-client.on("error", function(error) {
-  console.error(error);
-});
-
-client.set("key", "value", redis.print);
-client.get("key", redis.print);
-
 const port = 3000
 
 app.get('/', (req, res) => {
-    console.log(client.get("key", redis.print));
-    res.send({});
+    res.send({msg: "hello"});
 })
 
 app.listen(port, () => {
